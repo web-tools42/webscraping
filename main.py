3aca90ecf90b8187fe5efb05f2488ce2f8f85c9a
Bryan C Guner <66654881+bgoonz@users.noreply.github.com>
2022-01-21 00:35:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import turtle
+screen = turtle.Screen()
+
+matthew = turtle.Turtle()
+
+matthew.fillcolor("pink")
+matthew.begin_fill()
+for i in range(3):
+  matthew.right(90)
+  matthew.forward()
+matthew.end_fill()
+
+  
+
+  
